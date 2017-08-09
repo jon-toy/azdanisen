246e6c9ae047a97b17615778051de3beeecb9dd9
Jon Toy <jonathon.toy@gmail.com>
2017-08-09 00:14:05 -0700

    More attempts at Handlebars

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var handlebars = require('handlebars');
-
 var index = require('./routes/index');
 
 var app = express();
